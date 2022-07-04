b513634531510f28cb6d65b5573d754d67413495
NVMate <denys.miand@nure.ua>
2022-07-04 04:11:33 +0300

    First working version

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,5 +1,55 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import { cities as cities } from "./cities.js";
 
 export default function App() {
-  
+  const [city, setCity] = useState("Kyiv");
+  const API_KEY = "4c8da435efebfb2bcf3b7d861165f3c9";
+  function chooseCity(e) {
+    setCity(e.target.value);
+  }
+  const loadData = async () => {
+    const cityData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},ua&appid=${API_KEY}&units=metric`
+    );
+    return cityData;
+  };
+
+  const [airTemp, setAirTemp] = useState("Unknown");
+  const [humidity, setHumidity] = useState("Unknown");
+  const [pressure, setPressure] = useState("Unknown");
+  const [tempMax, setTempMax] = useState("Unknown");
+  const [tempMin, setTempMin] = useState("Unknown");
+
+  return (
+    <>
+      <select onChange={(e) => chooseCity(e)}>
+        {cities.map((misto) => (
+          <option key={misto.id} value={misto.name}>
+            {misto.name}
+          </option>
+        ))}
+      </select>
+      <button
+        onClick={() =>
+          loadData()
+            .then((weather) => {
+              let sunriseTime = new Date(weather.data.sys.sunrise);
+              let sunsetTime = new Date(weather.data.sys.sunset);
+              setAirTemp(weather.data.main.temp);
+              setHumidity(weather.data.main.humidity);
+              setPressure(weather.data.main.pressure);
+              setTempMax(weather.data.main.temp_max);
+              setTempMin(weather.data.main.temp_min);
+            })
+            .catch((error) => {
+              console.log(error);
+            })
+        }
+      >
+        Show Weather
+      </button>
+    </>
+  );
 }
